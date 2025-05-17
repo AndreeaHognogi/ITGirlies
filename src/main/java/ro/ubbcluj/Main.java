c68ba49e1980939533d255d10690acec221c7f93
andreea <adeacory@gmail.com>
2025-05-17 10:34:02 +0300

    Initial commit

---
+++ src/main/java/ro/ubbcluj/Main.java
@@ -0,0 +1,15 @@
+package ro.ubbcluj;
+
+import java.util.function.IntFunction;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf(String.valueOf((int)-4.3));
+
+
+    }
+}
